497d668aa20c9f58c857c53e6e3f92ede684d642
Michał <kamykowsky@gmail.com>
2023-10-03 14:25:36 +0200

    translation update

---
+++ lang/pl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Michał <kamykowsky@gmail.com>
+ */
+$lang['allowrename']           = 'Zezwól na zmienianie nazw stron i plików tych grup i użytkowników (oddzielane przecinkami)';
