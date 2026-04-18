42a7cc2dedc88bd11599adf52145d7cdeac71ae0
Marek Adamski <fevbew@wp.pl>
2026-04-19 01:55:33 +0200

    Translation update (pl)

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -6,18 +6,23 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @auther     Wojciech Kazimierczak <w.kazimierczak@gmail.com>
  *
+ * @author Marek Adamski <fevbew@wp.pl>
  * @author Marcin <mq4958@gmail.com>
  * @author Gary Owen
  */
 $lang['menu']                  = 'Przenieś / Zmień nazwę strony';
+$lang['inprogress']            = '(przeniesienie w toku)';
+$lang['treelink']              = 'Alternatywą dla tego prostego formularza jest możliwość zarządzania złożonymi restrukturyzacjami Twojej wiki przy użyciu <a href="%s">menedżera przenoszenia opartego na drzewie</a>.';
 $lang['notexist']              = 'Strona %s nie istnieje';
 $lang['norights']              = 'Nie masz wystarczających uprawnień do edycji %s.';
 $lang['filelocked']            = 'Ten plik jest zablokowany - spróbuj ponownie później';
 $lang['notchanged']            = 'Nie podano nowego miejsca docelowego dla strony %s (lokalizacja nie została zmieniona).';
+$lang['exists']                = 'Strony %s nie można przenieść do %s, cel już istnieje.';
 $lang['notargetperms']         = 'Nie masz uprawnień do utworzenia strony %s.';
 $lang['medianotexist']         = 'Plik multimedialny %s nie istnieje';
 $lang['nomediarights']         = 'Nie masz wystarczających uprawnień do usunięcia %s.';
 $lang['medianotchanged']       = 'Nie podano nowego miejsca docelowego dla strony %s (lokalizacja pozostała niezmieniona).';
+$lang['mediaexists']           = 'Nie można przenieść pliku multimedialnego %s do %s, cel już istnieje.';
 $lang['nomediatargetperms']    = 'Nie masz uprawnień do utworzenia pliku multimedialnego %s.';
 $lang['extensionchange']       = 'Rozszerzenia plików nie są identyczne.';
 $lang['indexerror']            = 'Błąd podczas aktualizacji indeksu wyszukiwania %s';
@@ -25,6 +30,7 @@ $lang['metamoveerror']         = 'Nie można przenieść plików meta strony %s'
 $lang['atticmoveerror']        = 'Nie można przenieść plików attic strony %s. Przenieś je ręcznie.';
 $lang['mediametamoveerror']    = 'Nie można przenieść plików meta pliku multimedialnego %s';
 $lang['mediamoveerror']        = 'Przenoszenie pliku multimedialnego %s nie powiodło się';
+$lang['mediaatticmoveerror']   = 'Nie udało się przenieść plików strychowych pliku multimedialnego %s. Przenieś je ręcznie. ';
 $lang['renamed']               = 'Nazwa strony została zmieniona z %s na %s';
 $lang['moved']                 = 'Strona została przeniesiona z %s do %s';
 $lang['move_rename']           = 'Strona została przeniesiona i zmieniono jej nazwę z %s na %s';
@@ -35,7 +41,32 @@ $lang['preview']               = 'Podgląd zmian do wykonania.';
 $lang['inexecution']           = 'Poprzedni ruch nie został ukończony - użyj poniższych przycisków, aby kontynuować lub przerwać jego wykonywanie.';
 $lang['btn_start']             = 'Rozpocznij';
 $lang['btn_continue']          = 'Kontynuuj';
+$lang['btn_retry']             = 'Ponów element';
+$lang['btn_skip']              = 'Pomiń element';
+$lang['btn_abort']             = 'Przerwij';
+$lang['legend']                = 'Przenieś bieżącą stronę lub przestrzeń nazw';
+$lang['movepage']              = 'Przenieś stronę';
+$lang['movens']                = 'Przenieś przestrzeń nazw';
+$lang['dst']                   = 'Nowa nazwa:';
+$lang['content_to_move']       = 'Zawartość do przeniesienia:';
+$lang['autoskip']              = 'Ignoruj ​​błędy i pomijaj strony lub pliki, których nie można przenieść';
+$lang['autorewrite']           = 'Przepisz linki zaraz po zakończeniu przenoszenia';
+$lang['move_pages']            = 'Strony';
+$lang['move_media']            = 'Pliki multimedialne';
+$lang['move_media_and_pages']  = 'Strony i pliki multimedialne';
+$lang['nodst']                 = 'Nie podano nowej nazwy';
+$lang['noaction']              = 'Nie określono żadnych przenoszeń';
+$lang['renamepage']            = 'Zmień nazwę strony';
+$lang['cantrename']            = 'Nie można teraz zmienić nazwy strony. Spróbuj później.';
 $lang['notallowed']            = 'Brak uprawnień do wykonania zmiany.';
+$lang['js']['rename']          = 'Zmień nazwę';
+$lang['js']['cancel']          = 'Anuluj';
+$lang['js']['newname']         = 'Nowa nazwa:';
+$lang['js']['inprogress']      = 'zmiana nazwy strony i dostosowanie linków...';
+$lang['js']['complete']        = 'Operacja przenoszenia zakończona.';
+$lang['js']['renameitem']      = 'Zmień nazwę tego elementu';
+$lang['js']['add']             = 'Utwórz nową przestrzeń nazw';
+$lang['js']['duplicate']       = 'Przepraszamy, „%s” już istnieje w tej przestrzeni nazw.';
 $lang['js']['moveButton']      = 'Przenieś/Zmień nazwę';
 $lang['js']['dialogIntro']     = 'Nowa nazwa pliku. Możesz zmienić katalog, ale nie rozszerzenie pliku.';
 $lang['root']                  = '[katalog bazowy]';
