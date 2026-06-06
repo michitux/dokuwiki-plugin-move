15de2facf25d5a22922c6f4067245099ba5c4616
Wizzard <wizzardsk@gmail.com>
2026-06-06 21:10:48 +0200

    Translation update (sk)

--- lang/sk/lang.php
+++ lang/sk/lang.php
@@ -1,39 +1,38 @@
 <?php
 
 /**
- * @author Viktor Kristian <vkristian@gmail.com>	
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ * @author Viktor Kristian <vkristian@gmail.com>
  */
-
 $lang['menu']                  = 'Presun/premenovanie stránky';
 $lang['inprogress']            = '(prebieha presun)';
 $lang['treelink']              = 'Ako alternatívu k tomuto jednoduchému formuláru môžte použiť <a href="%s">stromový manažér presuov</a> na komplexné preskupovanie vašej wiki.';
-
 $lang['notexist']              = 'Stránka \'%s\' neexistuje.';
 $lang['norights']              = 'Nedostatočné oprávnenie editovať \'%s\'.';
 $lang['filelocked']            = 'Stránka \'%s\' je uzamknutá - skúste prosím neskôr.';
 $lang['notchanged']            = 'Žiaden nový cieľ pre stránku \'%s\' nezadaný (umiestnenie nezmenené).';
 $lang['exists']                = 'Stránku \'%s\' nie je možné do \'%s\', cieľ už existuje.';
 $lang['notargetperms']         = 'Nemáte dostatočné oprávnenie vytvoriť stránku \'%s\'.';
-
 $lang['medianotexist']         = 'Médium \'%s\' neexistuje.';
 $lang['nomediarights']         = 'Nemáte dostatočné oprávnenie na zmazanie \'%s\'.';
 $lang['medianotchanged']       = 'Žiaden nový cieľ pre stránku \'%s\' nezadaný (umiestnenie nezmenené).';
 $lang['mediaexists']           = 'Médium \'%s\' nie je možné do \'%s\', cieľ už existuje.';
 $lang['nomediatargetperms']    = 'Nemáte dostatočné oprávnnenie vytvoriť médium \'%s\'.';
-
+$lang['extensionchange']       = 'Prípona nového súboru sa nezhoduje s pôvodnou.';
 $lang['indexerror']            = 'Chyba pri aktualizácii indexu vyhľadávania \'%s\'.';
 $lang['metamoveerror']         = 'Meta súbory stránky \'%s\' nemôžu byť presunuté.';
 $lang['atticmoveerror']        = 'Staré verzie stránky \'%s\' nemôžu byť presunuté. Prosím, presuňte ich ručne.';
 $lang['mediametamoveerror']    = 'Meta súbory média \'%s\' nemôžu byť presunuté.';
 $lang['mediamoveerror']        = 'Presunutie média \'%s\' zlyhalo.';
 $lang['mediaatticmoveerror']   = 'Staré verzie média \'%s\' nemôžu byť presunuté. Prosím, presuňte ich ručne.';
-
 $lang['renamed']               = 'Stránka premenovaná z \'%s\' na \'%s\'';
 $lang['moved']                 = 'Stránka presunutá z \'%s\' do \'%s\'';
 $lang['move_rename']           = 'Stránka presunutá a premenovaná z \'%s\' na \'%s\'';
 $lang['delete']                = 'Zmazané pluginem move';
 $lang['linkchange']            = 'Odkazy upravené z dôvodu presunu';
-
 $lang['intro']                 = 'Operácia presunu zatiaľ nezačala!';
 $lang['preview']               = 'Náhled zmien, ktoré budou vykonané.';
 $lang['inexecution']           = 'Predchádzajúci presun nebol dokončený - použite tlačidlá dole k pokračovaniu alebo zrušeniu presunu.';
@@ -42,7 +41,6 @@ $lang['btn_continue']          = 'Pokračovať';
 $lang['btn_retry']             = 'Znova';
 $lang['btn_skip']              = 'Preskočiť položku';
 $lang['btn_abort']             = 'Prerušiť';
-
 $lang['legend']                = 'Presunúť aktuálnu stránku nebo menný priestor';
 $lang['movepage']              = 'Presunúť stránku';
 $lang['movens']                = 'Presunúť aktuálny menný prostor';
@@ -55,18 +53,19 @@ $lang['move_media']            = 'Médiá';
 $lang['move_media_and_pages']  = 'Stránky a médiá';
 $lang['nodst']                 = 'Nebolo zadané nové meno';
 $lang['noaction']              = 'Neboli definované žiadne presuny';
-
 $lang['renamepage']            = 'Premenovať stránku';
 $lang['cantrename']            = 'Stránka nemôže byť momentálne premenovaná. Prosím skúste neskôr.';
+$lang['notallowed']            = 'Nemáte oprávnenie premenovávať stránky ani médiá.';
 $lang['js']['rename']          = 'Premenovať';
 $lang['js']['cancel']          = 'Zrušit';
 $lang['js']['newname']         = 'Nový názov:';
 $lang['js']['inprogress']      = 'Presúvam stránky a upravujem odkazy...';
 $lang['js']['complete']        = 'Presun bol dokončený.';
-
-$lang['root']                  = '[Koreňový menný priestor]';
-$lang['noscript']              = 'Táto vlastosť vyžaduje JavaScript';
-$lang['moveinprogress']        = 'Práve prebieha iná operácia presunu, tento nástroj momentálne nemôžete použiť.';
 $lang['js']['renameitem']      = 'Premenovať túto položku';
 $lang['js']['add']             = 'Vytvoriť nový menný priestor';
 $lang['js']['duplicate']       = 'Ľutujeme, \'%s\' už v tomto mennom priestore existuje.';
+$lang['js']['moveButton']      = 'Presunúť súbor';
+$lang['js']['dialogIntro']     = 'Zadajte nové umiestnenie súboru. Môžete zmeniť menný priestor, ale nie príponu súboru.';
+$lang['root']                  = '[Koreňový menný priestor]';
+$lang['noscript']              = 'Táto vlastosť vyžaduje JavaScript';
+$lang['moveinprogress']        = 'Práve prebieha iná operácia presunu, tento nástroj momentálne nemôžete použiť.';
