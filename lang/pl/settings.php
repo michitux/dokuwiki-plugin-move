dc7f11c82a691d069b2d3fc7a7607f8ca51375e2
Marek Adamski <fevbew@wp.pl>
2026-06-19 06:10:49 +0200

    Translation update (pl)

---
+++ lang/pl/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Marek Adamski <fevbew@wp.pl>
+ */
+$lang['allowrename']           = 'Zezwól na zmianę nazw stron i multimediów dla tych grup i użytkowników (rozdzielonych przecinkami).';
+$lang['minor']                 = 'Oznaczyć zmiany odnośników jako drobne? Drobne zmiany nie będą uwzględniane w kanałach RSS ani w mailach subskrypcyjnych.';
+$lang['dual']                  = 'Czy strony i pliki multimedialne powinny być wyświetlane osobno? Jeśli ta opcja jest wyłączona, ich przestrzenie nazw są wyświetlane tak, jakby stanowiły jedno drzewo.';
+$lang['autoskip']              = 'Włącz automatyczne pomijanie błędów przy przenoszeniu przestrzeni nazw.';
+$lang['autorewrite']           = 'Domyślnie włącz automatyczne przepisywanie odnośników po przeniesieniu przestrzeni nazw.';
+$lang['pagetools_integration'] = 'Dodaj przycisk zmiany nazwy do narzędzi strony';
